dc1d6d74f23bc44215af7a8ea9cd5f85cc0d1504
ruandinat <d.ruan@aluno.ifsp.edu.br>
2025-09-28 22:24:27 -0300

    primeiro commit

---
+++ src/test/java/com/ifsp/marmitaria/MarmitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.marmitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
